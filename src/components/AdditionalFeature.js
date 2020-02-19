65693f1ff9ad5bc31096ef2d84c9f1b24febd9b1
Josef (Joey) Etheridge <56942351+josefeth@users.noreply.github.com>
2020-02-19 06:48:42 -0700

    mvp and stretch

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Button } from 'react-bulma-components';
 
 const AdditionalFeature = props => {
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      
+
+      <Button color="danger" onClick={() => props.buyItem(props.feature)} > Add</Button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
 export default AdditionalFeature;
+
